e27a2671a2da7381278e5d84aae22146e5778d81
Alex Walker <alexwalker2003master@gmail.com>
2025-09-08 22:14:12 +0100

    Added Portal

--- mortgage-calculator/src/components/index.js
+++ mortgage-calculator/src/components/index.js
@@ -1,9 +1,14 @@
 import NavigationContext from "./navigation/navigationContext";
 import Navigation from "./navigation/navigation";
 import Breadcrumbs from "./navigation/breadcrumbs";
+import Portal from "./calculator/portal";
 
 export {
     NavigationContext,
     Navigation,
     Breadcrumbs,
+    
+    // Calculator Components
+    Portal,
+
 }
