cb1a383c6ac68665f04371bed4d7b8727e27f24f
Alex Walker <alexwalker2003master@gmail.com>
2025-09-08 20:25:01 +0100

    Nav Context Complete

--- mortgage-calculator/src/components/navigationContext.jsx
+++ mortgage-calculator/src/components/navigationContext.jsx
@@ -6,15 +6,27 @@ export default function NavigationContext(){
 
     const [curSelection, setCurrentSelection] = useState("Personal"); //define colour UI palette update with current selection
 
+    // No navigation functionality as this is for forage and its a clone of my wireframe for the lloyds bank template
+
+    const handleBackground = (selection) =>{
+        // Change background colour of button to indicate current selection
+
+        if(curSelection === selection){
+            return "bg-[var(--lloyds-green)] text-black";
+        }
+
+        return "bg-[var(--lloyds-black)] text-white";
+    }   
+
     return(
         <>
             <nav class="w-[100%] min-h-[50px] bg-[var(--lloyds-black)] 
             flex flex-row 2xl:px-32 sm:px-6 px-0 items-center sm:gap-x-8 overflow-x-hidden 
-            text-white font-semibold 2xl:text-xl sm:text-md text-sm text-center underline-offset-4">
-                <button class="bg-[var(--lloyds-black)] min-h-full px-4 max-w-[25%] hover:underline decoration-1">Personal</button>
-                <button class="bg-[var(--lloyds-black)] min-h-full px-4 max-w-[25%] hover:underline decoration-1">Business</button>
-                <button class="bg-[var(--lloyds-black)] min-h-full px-4 max-w-[25%] hover:underline decoration-1">Private Banking</button>
-                <button class="bg-[var(--lloyds-black)] min-h-full px-4 max-w-[25%] hover:underline decoration-1">International Banking</button>
+             font-semibold 2xl:text-lg sm:text-md text-sm text-center underline-offset-4">
+                <button class={`${handleBackground("Personal")} h-[50px] px-4 max-w-[25%] hover:underline decoration-1`}>Personal</button>
+                <button class={`${handleBackground("Business")} min-h-full px-4 max-w-[25%] hover:underline decoration-1`}>Business</button>
+                <button class={`${handleBackground("PrivateBanking")} min-h-full px-4 max-w-[25%] hover:underline decoration-1`}>Private Banking</button>
+                <button class={`${handleBackground("InternationalBanking")} min-h-full px-4 max-w-[25%] hover:underline decoration-1`}>International Banking</button>
             </nav>
         </>
     )
