89216061c95a9e3245939e51215cee1d23a13f61
Alex Walker <alexwalker2003master@gmail.com>
2025-09-09 04:57:37 +0100

    UX Update

--- mortgage-calculator/src/components/calculator/helper.jsx
+++ mortgage-calculator/src/components/calculator/helper.jsx
@@ -10,13 +10,13 @@ export default function Helper({title, description}){
 
     return(
         <>
-            <p onMouseEnter={() => {toggleHelp(true)}}
+            <p
              className="ml-2 text-3xl font-[400] text-[var(--lloyds-blue)]"><span onClick={() => {toggleHelp(!showHelp)}} className="cursor-pointer">ⓘ</span>
 
                 <span
                 className={`${showHelp ? "block" : "hidden"} w-[100%] left-0 sm:w-[300px] sm:left-auto sm:right-40 mt-4  z-[2] p-2 text-sm rounded-md drop-shadow-[0_0_3px_var(--lloyds-black)]
-                min-h-auto bg-[var(--lloyds-dark-green)] absolute text-white`}>
-                    <strong>{title}<span class="float-end">✗</span></strong>
+                min-h-auto bg-[var(--lloyds-black)] absolute text-white`}>
+                    <strong>{title}<span class="float-end cursor-pointer hover:opacity-70 text-xl -mt-1" onClick={() => {toggleHelp(false)}}>✗</span></strong>
                     <br/><br/>{description}
                 </span>
             </p>
