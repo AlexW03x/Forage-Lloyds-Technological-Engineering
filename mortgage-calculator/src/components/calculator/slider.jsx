1f8d571bdb7e50a0e3fdabbc1ce698f161b4ffe2
Alex Walker <alexwalker2003master@gmail.com>
2025-09-09 17:18:42 +0100

    Finalised Slider Input

--- mortgage-calculator/src/components/calculator/slider.jsx
+++ mortgage-calculator/src/components/calculator/slider.jsx
@@ -14,7 +14,8 @@ export default function Slider({label="", values={min: 0, default: 25, max:100},
             <div className={`w-[95%] mx-auto flex flex-col sm:flex-row gap-2 sm:gap-4 text-[var(--lloyds-black)] sm:items-center ${classExtensions}`}>
                 <p className="font-semibold text-md sm:w-[150px] sm:text-right">{label}</p>
                 <input type="range" min={values.min} max={values.max} step={1}
-                className="rounded-md h-[40px] sm:max-w-[300px] w-[100%] px-2 outline-none drop-shadow-[0_0_1px_black] focus:drop-shadow-[0_0_2px_green]">
+                className="rounded-md h-[40px] sm:max-w-[300px] w-[100%]  outline-none drop-shadow-[0_0_1px_black] focus:drop-shadow-[0_0_2px_green]
+                hue-rotate-[150deg] saturate-200">
                 </input>
 
                 <div className="sm:max-w-[300px] sm:w-[100%] w-[95%] absolute h-[20px] sm:ml-[165px] sm:mt-10 mt-16">
