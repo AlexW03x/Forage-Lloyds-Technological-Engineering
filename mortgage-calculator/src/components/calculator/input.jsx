9a2c40d45fb48631edbac70322bba44256ca91cc
Alex Walker <alexwalker2003master@gmail.com>
2025-09-09 16:47:15 +0100

    Pagination Added

--- mortgage-calculator/src/components/calculator/input.jsx
+++ mortgage-calculator/src/components/calculator/input.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 
-export default function Input({type, label, placeholder, onChange=null}){
+
+//@params type == input field type so text, password, email, etc
+//@params label == String: title for input field
+//@params placeholder == String: placeholder text for label
+//@params onChange == Void: function to update e.target.value
+//@params classExtensions == String: add more classNames to div container
+export default function Input({type, label, placeholder, onChange=null, classExtensions=null}){
 
     const updateValues = (input) => {
         onChange(input); //pass values onto different component variables
@@ -8,7 +14,7 @@ export default function Input({type, label, placeholder, onChange=null}){
 
     return(
         <>
-            <div className="w-[95%] mx-auto flex flex-col sm:flex-row gap-2 sm:gap-4 text-[var(--lloyds-black)] sm:items-center mt-4">
+            <div className={`w-[95%] mx-auto flex flex-col sm:flex-row gap-2 sm:gap-4 text-[var(--lloyds-black)] sm:items-center ${classExtensions}`}>
                 <p className="font-semibold text-md sm:w-[150px] sm:text-right">{label}</p>
                 <input type={type} placeholder={placeholder} 
                 className="rounded-md h-[40px] sm:max-w-[300px] w-[100%] px-2 outline-none drop-shadow-[0_0_1px_black] focus:drop-shadow-[0_0_2px_green]">
--- mortgage-calculator/src/components/calculator/portal.jsx
+++ mortgage-calculator/src/components/calculator/portal.jsx
@@ -41,17 +41,35 @@ export default function Portal(){
                         {/* Calculator content will go here */}
                         <Notice logo={texts.Calculator_Notice.logo} description={texts.Calculator_Notice.description}/>
 
-                        <Input type="text" label="Property Value (£):" placeholder="Enter Property Value (£)"></Input>
-                        <Input type="text" label="Your Deposit (£):" placeholder="Enter Deposit Amount (£)"></Input>
+                        <Input type="text" label="Property Value (£):" placeholder="Enter Property Value (£)" 
+                        classExtensions={"mt-10 flex justify-center -ml-1"}></Input>
+                        
+                        <Input type="text" label="Your Deposit (£):" placeholder="Enter Deposit Amount (£)"
+                        classExtensions={"mt-4 flex justify-center -ml-1"}></Input>
                         
                     </div>
 
-                    <div className="w-full h-[60px] flex flex-row items-center justify-center font-semibold text-lg">
-                        <p className="absolute text-[var(--lloyds-black)] bottom-1 left-3 text-xs flex flex-row">Safe & Secure 
-                            <img src={Lock} alt="Lock Icon" className="object-contain w-[15px] h-[15px] ml-1"></img>
+                    <div className="w-full h-[60px] flex flex-row items-center justify-center font-semibold">
+                        <p className="absolute text-[var(--lloyds-black)] bottom-1 left-1 text-[0.6rem] flex flex-row">Safe & Secure 
+                            <img src={Lock} alt="Lock Icon" className="object-contain w-[12px] h-[12px] ml-1"></img>
                         </p>
 
-                        
+                        <div className="" id="pagination">
+                            <button onClick={() => {}} className="w-[30px] h-[30px] rounded-md bg-[var(--lloyds-light-grey)]
+                                  hover:bg-black/20 defaultTransition rotate-180">
+                                    ➤
+                            </button>
+
+                            <select value="" className="bg-transparent text-md ml-2 mr-2">
+                                <option value="1">Page 1 / 2</option>
+                            </select>
+
+                            <button onClick={() => {}} className="w-[30px] h-[30px] rounded-md bg-[var(--lloyds-light-grey)]
+                                 hover:bg-black/20 defaultTransition">
+                                    ➤
+                            </button>
+                        </div>
+
                     </div>
             </div>
         </>
