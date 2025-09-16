df95f28c21679b7c7f0fbafc0609ba9314fb5077
Alex Walker <alexwalker2003master@gmail.com>
2025-09-16 21:40:16 +0100

    Added interest only

--- mortgage-calculator/src/components/calculator/portalPages/page3.jsx
+++ mortgage-calculator/src/components/calculator/portalPages/page3.jsx
@@ -3,41 +3,46 @@ import Notice from "../notice";
 import Input from "../input";
 import CircularProgress from "../circularProgress";
 import Helper from "../helper";
+import Slider from "../slider";
 
 export default function Page3(
     {functionToUpdate=null}
 ){
 
     const [information, setInformation] = useState(""); //tell users about their current page
-    const [prevInput, setPrevInput] = useState(""); //holds historic session cached information of user input
     const [pathway, setPathway] = useState(""); //holds pathway information for the user tracking
     const [error, setError] = useState(""); //for if there is anything that isn't filled.
     const [loanToValue, setLTV] = useState(0.00); //for showing the loan to value percentage
+    const [mortgageTerms, setMortgageTerms] = useState("");
+    const [propertyValue, setPropertyValue] = useState("");
+    const [depositAmount, setDepositAmount] = useState("");
+    const [loanAmount, setLoanAmount] = useState("");
     
     //for input tracking
     const [interestRate, setInterestRate] = useState(0.00); //float
+    const [interestPeriod, setInterestPeriod] = useState(5);
 
     useEffect(() => { //fetch historic session cache for user tracking
         try{
-            let propertyValue = sessionStorage.getItem("propertyValue");
-            let depositAmount = sessionStorage.getItem("depositAmount");
+            let property_Value = sessionStorage.getItem("propertyValue");
+            setPropertyValue(property_Value);
+            let deposit_Amount = sessionStorage.getItem("depositAmount");
+            setDepositAmount(deposit_Amount);
             let loanValue = "";
             if(sessionStorage.getItem("loanAmount")){
                 loanValue = sessionStorage.getItem("loanAmount"); //auto calculate if doesn't exist
+                setLoanAmount(loanValue);
             }
             else{
                 loanValue = propertyValue - depositAmount;
             }
-            let mortgageTerms = sessionStorage.getItem("mortgageTerms");
-            setPathway(sessionStorage.getItem("pathway"));
-            let loan_to_value = (parseFloat(loanValue) / parseFloat(propertyValue)) * 100;
-            setLTV(loan_to_value);
-            setInformation(`Please select the mortgage type you would like to calculate!`);
-            setPrevInput(
-                `Property Value: £${propertyValue} 🔹 Depositing: £${depositAmount} 🔹 Loaning: £${loanValue} 🔹 
-                Terms: ${mortgageTerms} Years`
-            );
+            let mortgage_Terms = sessionStorage.getItem("mortgageTerms");
+            setMortgageTerms(mortgage_Terms);
 
+            setPathway(sessionStorage.getItem("pathway"));
+            let loan_to_value = (parseFloat(loanValue) / parseFloat(property_Value)) * 100;
+            setLTV(loan_to_value);
+            setInformation(`Please fill in the remaining inputs to calculate your mortgage`);
 
         }
         catch{
@@ -45,18 +50,25 @@ export default function Page3(
         }
     }, []);
 
+
+    //for interest only functions
+    const updatePeriod = (e) => {
+        setInterestPeriod(Number(e));
+    }
+
     return(
         <>
-            <Notice logo="success" description="Please fill in the remaining inputs to calculate your mortgage" childrenNodes={
+            <Notice logo="success" description={information} childrenNodes={
                 <p className="text-[var(--lloyds-blue)] text-sm">
                     <span className="text-[var(--lloyds-black)]">Your current inputs:</span>
-                    <br/>{prevInput} 🔹 Type: {pathway}
+                    <br/>Property Value: £{propertyValue} 🔹 Depositing: £{depositAmount} 🔹 Loaning: £{loanAmount} 🔹 
+                Terms: {mortgageTerms} Years 🔹 Type: {pathway}
                 </p>
             }></Notice>
 
             {error != "" && <p className="mt-4 mb-2 text-center font-semibold mx-2 text-[var(--lloyds-red)]">{error}</p>}
             {(pathway.toLowerCase().includes("fixed") || pathway.toLowerCase().includes("only")) && 
-            <div>
+            <div className="z-[6]">
                 <Input type="text" label="Interest Rate (%): "
                 classExtensions={"mt-8 flex justify-center sm:-ml-1"}
                 placeholder="Any % from 0.00-20.00" 
@@ -70,11 +82,22 @@ export default function Page3(
             </div>
             }
 
-            <div className="flex flex-row mt-6 justify-center items-center sm:gap-4 gap-2">
+            {pathway.toLowerCase().includes("only") &&
+            <div>
+                <Slider 
+                    label="Interest Period:" 
+                    values={{min: 1, max: mortgageTerms, default: mortgageTerms}}
+                    classExtensions={"mt-4 flex justify-center sm:-ml-1"}
+                    value={interestPeriod} onChange={updatePeriod}
+                ></Slider>
+            </div>
+            }
+
+            <div className="flex flex-row mt-6 justify-center items-center sm:gap-4 gap-2 z-[5]">
                 <p className="font-semibold text-[var(--lloyds-black)] mx-2 sm:w-[150px] w-[250px] sm:text-right">Loan To Value:</p>
-                <div className="sm:max-w-[300px] w-[100%]">
+                <div className="sm:max-w-[300px] w-[100%] flex sm:justify-end justify-center">
                     <CircularProgress 
-                    percentage={loanToValue.toFixed(2)} size={100} strokeWidth={6}
+                    percentage={loanToValue.toFixed(2)} size={80} strokeWidth={6}
                     classExtensions={"text-[var(--lloyds-dark-green)]"}
                     textExtensions={"text-md font-semibold"}
                 />
@@ -87,6 +110,10 @@ export default function Page3(
                     customPos={"w-[300px] absolute right-0 mt-1 sm:-right-12"}
                 />
             </div>
+
+            <button onClick={() => {}} className="w-[200px] h-[40px] rounded-md text-[var(--lloyds-white)]
+                font-semibold bg-[var(--lloyds-dark-green)] mt-10 hover:opacity-70 focus:opacity-70
+                drop-shadow-[0_0_1px_black] mx-auto sm:translate-x-28">Next Page</button>
         </>
     )
 }
