2cd0e1cbe497ae2ec241506c2372899594df06a4
Alex Walker <alexwalker2003master@gmail.com>
2025-09-18 18:52:05 +0100

    Setting up page 4

--- mortgage-calculator/src/components/calculator/portalPages/page4.jsx
+++ mortgage-calculator/src/components/calculator/portalPages/page4.jsx
@@ -1,6 +1,32 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Page4(){
+    //variables for fetching every single user input if available
+    //standard
+    const [propertyValue, setPropertyValue] = useState("");
+    const [depositAmount, setDepositAmount] = useState("");
+    const [loanValue, setLoanValue] = useState("");
+    const [mortgageTerms, setMortgageTerms] = useState("");
+    
+    //pathway
+    const [pathway, setPathway] = useState("");
+    
+    //pathway variables
+    const [interestRate, setInterestRate] = useState("");
+    const [adjustmentFrequency, setAdjustmentFrequency] = useState("");
+    const [interestPeriod, setInterestPeriod] = useState("");
+
+
+    useEffect(() => {
+        try{
+
+        }
+        catch{
+            
+        }
+    })
+
+
     return(
         <>
         </>
